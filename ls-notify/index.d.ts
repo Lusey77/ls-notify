aea7e92879b0c8dec6fdd2a3b4fc1c90018e099f
Lusey77 <Lusey77@users.noreply.github.com>
2018-04-07 14:22:38 -0500

    Removed dist folder items

--- ls-notify/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './notify.component';
-export * from './notify.service';
-export declare class NotifyModule {
-    static forRoot(): ModuleWithProviders;
-    static forChild(): ModuleWithProviders;
-}
--- ls-notify/notify.severity.d.ts
+++
@@ -1,8 +0,0 @@
-declare type NotificationSeverity = 'success' | 'info' | 'warning' | 'error';
-declare const NotificationSeverity: {
-    readonly Success: NotificationSeverity;
-    readonly Info: NotificationSeverity;
-    readonly Warning: NotificationSeverity;
-    readonly Error: NotificationSeverity;
-};
-export { NotificationSeverity };
